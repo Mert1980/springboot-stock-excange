d47f5dc3d6ecf235c4b1d88e6393d7e31119653d
Mert <mertdemirok80@gmail.com>
2021-10-19 17:08:21 +0200

    add spring annotations and mapping in controller

--- api/src/main/java/com/switchfully/controller/StockExchangeController.java
+++ api/src/main/java/com/switchfully/controller/StockExchangeController.java
@@ -1,18 +1,29 @@
 package com.switchfully.controller;
 
-
 import com.switchfully.model.Stock;
 import com.switchfully.dto.StockDto;
 import com.switchfully.mapper.StockMapper;
 import com.switchfully.service.StockService;
-import com.switchfully.service.StockServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@ResponseBody
+@RequestMapping("/stocks")
 public class StockExchangeController {
 
-    StockService stockService = new StockServiceImpl();
+    private final StockService stockService;
+    private final StockMapper stockMapper;
 
-    public StockDto getStock(String stockId){
+    @Autowired
+    public StockExchangeController(StockService stockService, StockMapper stockMapper) {
+        this.stockService = stockService;
+        this.stockMapper = stockMapper;
+    }
+
+    @GetMapping("/{stockId}")
+    public StockDto getStock(@PathVariable String stockId){
         Stock stock = stockService.getStock(stockId);
-        return StockMapper.toDto(stock);
+        return stockMapper.toDto(stock);
     }
 }
